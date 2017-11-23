05106b5673d0e6dabe07e3d180578ac09060e8fa
Karl Veskus <karl.veskus@gmail.com>
2017-11-24 01:56:48 +0200

    Add input field for address

---
+++ web/static/js/address_field.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <input type="text" placeholder="Enter parking address here"/>
+        <button>Search</button>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style scoped>
+    div {
+        padding: 10px;
+        position: absolute;
+        width: 100%;
+        z-index: 1;
+    }
+
+    input {
+        background: #fff;
+        border: 1px solid #fff;
+        box-shadow: 0 4px 10px -2px rgba(0, 0, 0, .2);
+        box-sizing: border-box;
+        color: #929292;
+        float: left;
+        font: inherit;
+        outline: 0;
+        padding: 10px 10px;
+        width: 290px;
+    }
+
+    @media (max-width: 768px) {
+        input {
+            width: calc(100% - 70px);
+        }
+    }
+
+    input:active, input:focus {
+        border: 1px solid #4a80f5;
+        border-right: 0;
+    }
+
+    input::-webkit-input-placeholder {
+        color: #929292;
+    }
+
+    button {
+        background: #4a80f5;
+        border: 0;
+        box-shadow: 0 4px 10px -2px rgba(0, 0, 0, .2);
+        color: white;
+        float: left;
+        font-family: inherit;
+        font-size: 14px;
+        height: 42px;
+        outline: 0;
+        width: 70px;
+    }
+
+    button:hover {
+        background: #4273dc;
+        cursor: pointer;
+    }
+
+    button:active {
+        background: #3b66c4;
+    }
+</style>
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import "vueify/lib/insert-css";
 
 import parking_map from "./parking_map";
+import address_field from "./address_field";
+
 Vue.component("parking-map", parking_map);
+Vue.component("address-field", address_field);
 
 new Vue({}).$mount("#parking-app");
