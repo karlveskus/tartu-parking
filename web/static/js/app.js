b6263f0e38977bc44ace24788bc4d9280cae63ed
Karl Veskus <karl.veskus@gmail.com>
2017-11-23 23:37:42 +0200

    Add initial map

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
+import "vueify/lib/insert-css";
 
 import parking_map from "./parking_map";
-Vue.component("parking_map", parking_map);
+Vue.component("parking-map", parking_map);
 
-new Vue({}).$mount("#takso-app");
+new Vue({}).$mount("#parking-app");
--- web/static/js/parking_map.vue
+++ web/static/js/parking_map.vue
@@ -1,15 +1,31 @@
 <template>
-  <div>
-      Hello
-  </div>
+  <div class="google-map" :id="mapName"></div>
 </template>
 
 <script>
 export default {
-
-}
+  name: 'parking-map',
+  data: function () {
+    return {
+      mapName: this.name + "-map",
+    }
+  },
+  mounted: function () {
+    const element = document.getElementById(this.mapName)
+    const options = {
+      zoom: 14,
+      center: new google.maps.LatLng(58.378025,26.728493)
+    }
+    const map = new google.maps.Map(element, options);
+    
+  }
+};
 </script>
 
-<style>
-
+<style scoped>
+.google-map {
+  width: 100%;
+  height: 600px;
+  background: gray;
+}
 </style>
