7e8ecce96742820c444ab90995782e3f64200f98
Karl Veskus <karl.veskus@gmail.com>
2017-11-23 17:10:10 +0200

    Add Vue support

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,21 +1,6 @@
-// Brunch automatically concatenates all files in your
-// watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
-//
-// However, those files will only be executed if
-// explicitly imported. The only exception are files
-// in vendor, which are never wrapped in imports and
-// therefore are always executed.
+import Vue from "vue";
 
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
-import "phoenix_html"
+import parking_map from "./parking_map";
+Vue.component("parking_map", parking_map);
 
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
-
-// import socket from "./socket"
+new Vue({}).$mount("#takso-app");
