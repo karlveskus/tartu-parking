bc31893683a955eebad2edabef73035948fc6646
Karl Veskus <karl.veskus@gmail.com>
2017-12-03 01:59:00 +0200

    Concat map and inputfield components

--- web/static/js/parking_map.vue
+++ web/static/js/parking_map.vue
@@ -1,15 +1,48 @@
 <template>
-    <div class="google-map" :id="mapName"></div>
+    <div class="wrapper">
+        <div class="input-field">
+            <input v-model="destionation_address" v-on:keyup.enter="get_parkings" type="text" placeholder="Enter parking address here"/>
+            <button v-on:click="get_parkings">Search</button>
+        </div>
+        <div class="google-map" :id="mapName"></div>
+    </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
     data: function () {
         return {
             geocoder: new google.maps.Geocoder(),
-            mapName: 'parking-map',
-            map: undefined,
-            address: this.$store.getters.address
+            mapName: '#parking-map',
+            map: null,
+            destionation_address: "",
+            destionation_marker: null
+        }
+    },
+    methods: {
+        get_parkings: function() {
+            // Remove old marker if exists
+            if (this.destionation_marker !== null) this.destionation_marker.setMap(null)
+
+            let url = "/api/parkings?address=" + this.destionation_address
+
+            axios.get(url)
+            .then((res) => console.log(res.data))
+
+            this.geocoder.geocode({'address': this.destionation_address}, (results, status) => {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    this.map.setCenter(results[0].geometry.location);
+                    this.destionation_marker = new google.maps.Marker({
+                        map: this.map,
+                        position: results[0].geometry.location
+                    });
+                } else {
+                    console.log('Geocode was not successful for the following reason: ' + status);
+                }
+            });
+
         }
     },
     mounted: function () {
@@ -24,26 +57,81 @@ export default {
                 fullscreenControl: false
             }
         );
-
-        this.geocoder.geocode({'address': this.address}, (results, status) => {
-            if (status == google.maps.GeocoderStatus.OK) {
-                this.map.setCenter(results[0].geometry.location);
-                var marker = new google.maps.Marker({
-                    map: this.map,
-                    position: results[0].geometry.location
-                });
-            } else {
-                console.log('Geocode was not successful for the following reason: ' + status);
-            }
-        });
     }
 };
 </script>
 
-<style scoped>
-.google-map {
-    width: 100%;
+<style scoped lang="scss">
+div.wrapper {
     height: 100%;
-    background: gray;
+
+    .input-field {
+        padding: 10px;
+        position: absolute;
+        width: 100%;
+        z-index: 1;
+
+        input {
+            -webkit-appearance: none;
+            -moz-appearance: none;
+            appearance: none;
+
+            background: #fff;
+            border: 1px solid #fff;
+            border-radius: 0;
+            box-shadow: 0 2px 6px 0px rgba(0, 0, 0, .2);
+            box-sizing: border-box;
+            color: #929292;
+            float: left;
+            font: inherit;
+            margin: 0;
+            outline: 0;
+            padding: 10px 10px;
+            width: 290px;
+
+            &:active, &:focus {
+                border: 1px solid #4a80f5;
+                border-right: 0;
+            }
+
+            &::-webkit-input-placeholder {
+                color: #929292;
+            }
+        }
+
+        @media (max-width: 768px) {
+            input {
+                width: calc(100% - 70px);
+            }
+        }
+
+        button {
+            background: #4a80f5;
+            border: 0;
+            box-shadow: 0 4px 10px -2px rgba(0, 0, 0, .2);
+            color: white;
+            float: left;
+            font-family: inherit;
+            font-size: 14px;
+            height: 42px;
+            outline: 0;
+            width: 70px;
+
+            &:hover {
+                background: #4273dc;
+                cursor: pointer;
+            }
+
+            &:active {
+                background: #3b66c4;
+            }
+        }
+    }
+
+    .google-map {
+        width: 100%;
+        height: 100%;
+        background: gray;
+    }
 }
 </style>
