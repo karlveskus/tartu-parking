e3b4bd134ac4f22f83245dabda296bf95d37adb5
Karl Veskus <karl.veskus@gmail.com>
2017-12-03 05:07:07 +0200

    Fix button shadow

--- web/static/js/parking_map.vue
+++ web/static/js/parking_map.vue
@@ -181,7 +181,7 @@ div.wrapper {
         button {
             background: #4a80f5;
             border: 0;
-            box-shadow: 0 4px 10px -2px rgba(0, 0, 0, .2);
+            box-shadow: 0 2px 6px 0px rgba(0, 0, 0, .2);
             color: white;
             float: left;
             font-family: inherit;
