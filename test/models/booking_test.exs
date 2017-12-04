b96fe784560ef8810bc9213813b7c16f54420199
Karl Veskus <karl.veskus@gmail.com>
2017-12-04 05:58:21 +0200

    Fix some tests

--- test/models/booking_test.exs
+++ test/models/booking_test.exs
@@ -4,15 +4,10 @@ defmodule TartuParking.BookingTest do
   alias TartuParking.Booking
 
   @valid_attrs %{}
-  @invalid_attrs %{}
 
   test "changeset with valid attributes" do
     changeset = Booking.changeset(%Booking{}, @valid_attrs)
     assert changeset.valid?
   end
-
-  test "changeset with invalid attributes" do
-    changeset = Booking.changeset(%Booking{}, @invalid_attrs)
-    refute changeset.valid?
-  end
+  
 end
