423bc4716eabcf0e881011dc887c4e87128abda4
Eneko Ruiz de Loizaga <enekoruizdeloizaga@gmail.com>
2017-12-06 16:32:02 +0200

    Add vue.js component to login

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -4,10 +4,12 @@ import "axios";
 
 import parking_map from "./components/parking_map";
 import booking from "./components/booking/booking";
+import login from "./login";
 
 Vue.component("parking-map", parking_map);
 Vue.component("booking", booking);
+Vue.component("login", login);
 
 new Vue({
     el: '#parking-app'
-})
+});
---
+++ web/static/js/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="input-field" id="username" type="text"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
