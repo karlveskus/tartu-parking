a5b95672e767945646f728e7a6b1e58f484b7182
gsheklashvili <Giorgi.Sheklashvili@tudeng.ut.ee>
2017-12-14 04:11:42 +0200

    add feature files for login and booking

---
+++ features/contexts/booking_slot_context.exs
@@ -0,0 +1,8 @@
+defmodule WhiteBreadbooking_slot_context do
+    use WhiteBread.Context
+    use Hound.Helpers
+    alias TartuParking.{Repo,Parking,DataParser}
+    alias Ecto.{Changeset}
+  
+  end
+  
---
+++ features/contexts/login_context.exs
@@ -0,0 +1,8 @@
+defmodule WhiteBreadlogin_context do
+    use WhiteBread.Context
+    use Hound.Helpers
+    alias TartuParking.{Repo,Parking,DataParser}
+    alias Ecto.{Changeset}
+  
+  end
+  
